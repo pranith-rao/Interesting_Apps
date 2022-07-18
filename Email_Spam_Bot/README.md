4acc95dd8c0b07a01bdc0faa65d6d4417d2155bb
Pranith Rao <pranithrao3@gmail.com>
2022-07-18 22:04:33 +0530

    email spam

---
+++ Email_Spam_Bot/README.md
@@ -0,0 +1,6 @@
+# email-spam-bot  
+https://email-spam-bot.herokuapp.com/  
+Due to free hosting spam is limited 20-30 on this website   
+Somtimes even higher like 100 is possible  
+
+But code works absolutely fine for any number  
---
+++ Email_Spam_Bot/app.py
@@ -0,0 +1,42 @@
+from flask import Flask,render_template,request,flash,redirect,url_for
+import os
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
+from sendgrid.helpers.mail import To
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.urandom(16)
+
+def send_mail(recepient,mail_object,body):
+    message = Mail(
+    from_email=("bulkmailer69@gmail.com", "Spammer"),
+    to_emails=recepient,
+    subject=mail_object,
+    html_content=body)
+    sg = SendGridAPIClient(
+    "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")
+    sg.send(message)
+
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+@app.route('/send_message',methods=['POST'])
+def send_message():
+    if request.method == "POST":
+        email = request.form["email"]
+        subject = request.form["subject"]
+        message = request.form["msg"]
+        N = int(request.form["numberof"])
+        try:
+            for _ in range(1,N):
+                send_mail(email,subject,message)
+            flash("Spammed","success")
+            return redirect(url_for("home"))
+        except:
+            flash("Spammed failed.Please try again later","error")
+            return redirect(url_for("home"))
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
